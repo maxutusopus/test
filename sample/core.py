a9aa35021e1258f17a66e74f85596b076d4dd04f
Maxim <info@mosermax.de>
2018-05-26 19:29:03 +0200

    fff

--- sample/core.py
+++ sample/core.py
@@ -1,27 +1,42 @@
-from pymongo import MongoClient
-#from db_connections
+# from pymongo import MongoClient
 
-
+from sample import db_connections as m
 import pprint
 
-# class MongoDBConnection(object):
-#     def __init__(self, host='localhost', port=27017):
-#         self.host = host
-#         self.port = port
-#         self.connection = None
-#
-#     def __enter__(self):
-#         self.connection = MongoClient(self.host, self.port)
-#         return self
-#
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         self.connection.close()
+import time
 
 
-mongo = MongoDBConnection()
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+
+        print('Elapsed time:{}'.format(te-ts))
+
+        return result
+
+    return timed
+
+@timeit
+def test():
+    w = 1 + 1
+    print(w)
+
+
+test()
+
+mongo = m.MongoDBConnection()
 with mongo:
     collection = mongo.connection.arztdata.arzt
-#    arzt = collection.find({'_id': '5729c54ad285a8120c4ef1c5'})
-#    print(arzt)
-    for doc in collection.find_one({'ll_arzt_id': 'A199291'}):
-        pprint.pprint(dir(doc))
+    #    arzt = collection.find({'_id': '5729c54ad285a8120c4ef1c5'})
+    #    print(arzt)
+    result = ()
+    for i, doc in enumerate(collection.find()):
+        # print(i, doc['nachname'])
+        result = i
+
+    print(result)
+
+
