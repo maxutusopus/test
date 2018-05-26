383a4df170eaa23227073389a9e0977c8daf7e17
Maxim <info@mosermax.de>
2018-05-26 18:17:08 +0200

    fff

---
+++ sample/core.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+import pprint
+
+class MongoDBConnection(object):
+    def __init__(self, host='localhost', port=27017):
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+
+mongo = MongoDBConnection()
+with mongo:
+    collection = mongo.connection.arztdata.arzt
+#    arzt = collection.find({'_id': '5729c54ad285a8120c4ef1c5'})
+#    print(arzt)
+    for doc in collection.find_one({'ll_arzt_id': 'A199291'}):
+        pprint.pprint(dir(doc))
---
+++ sample/db_connections.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+import pprint
+
+class MongoDBConnection(object):
+    def __init__(self, host='localhost', port=27017):
+        self.host = host
+        self.port = port
+        self.connection = None
+
+    def __enter__(self):
+        self.connection = MongoClient(self.host, self.port)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.connection.close()
+
+###################   SQL
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class SQLAlchemyDBConnection(object):
+    """SQLAlchemy database connection"""
+    def __init__(self, connection_string):
+        self.connection_string = connection_string
+        self.session = None
+    def __enter__(self):
+        engine = create_engine(self.connection_string)
+        Session = sessionmaker()
+        self.session = Session(bind=engine)
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
